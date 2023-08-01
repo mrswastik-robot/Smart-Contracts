f42687be5868f9d4203a9ca23f4547b088f5d461
swastik <swastikpatel29@gmail.com>
2023-08-01 23:38:56 +0530

    deploy scripts for mock & raffle done,  now verify

--- smartContract-lottery-hardhat/deploy/00-deploy-mock.js
+++ smartContract-lottery-hardhat/deploy/00-deploy-mock.js
@@ -2,6 +2,9 @@
 const { network } = require("hardhat");
 const {developmentChains,} = require("../helper-hardhat-config");
 
+const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
+const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const { deploy, log } = deployments;
@@ -11,7 +14,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     if(chainId == 31337 || chainId == 1337 ){
 
         // Deploy a mock vrf coordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+            waitConfirmations: 1,
+        }); 
+
+        log("Mocks deployed")
+        log("-------------------------------------")
     }
 
-
 };
+
+module.exports.tags = ["all", "mocks"];
--- smartContract-lottery-hardhat/deploy/01-deploy-raffle.js
+++ smartContract-lottery-hardhat/deploy/01-deploy-raffle.js
@@ -1,12 +1,50 @@
-const { network } = require("hardhat");
+
+const { network, ethers } = require("hardhat");
+const {developmentChains, networkConfig} = require("../helper-hardhat-config");
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
 
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    let VRFCoordinatorV2Address;
+    let subscriptionId;
+
+    if(developmentChains.includes(network.name))
+    {
+        const VRFCoordinatorV2 = await deployments.get("VRFCoordinatorV2Mock");
+        VRFCoordinatorV2Address = VRFCoordinatorV2.address;
+        const transactionResponse = await VRFCoordinatorV2.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
+        subscriptionId = transactionReceipt.events[0].args.subId;
+
+        //Funding the subscription
+        //usually you would use the LINK token faucet for this
+        await VRFCoordinatorV2.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT);
+    }
+    //now for main net or test nets 
+    else
+    {
+        VRFCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subscriptionId = networkConfig[chainId]["subscriptionId"];
+    }
+
+    log("-------------------------------------")
+    const arguments = [
+                        VRFCoordinatorV2Address,
+                        subscriptionId,
+                        networkConfig[chainId]["entranceFee"],
+                        networkConfig[chainId]["gasLane"],
+                        networkConfig[chainId]["callbackGasLimit"],
+                        networkConfig[chainId]["interval"],
+                        ]
+        
     
-    await deploy("Raffle", {
+    const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
         log: true,
