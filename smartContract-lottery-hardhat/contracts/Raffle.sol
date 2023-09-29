003c5688912fd17be0c3bf6b90d078dc2f8e8e0b
swastik <swastikpatel29@gmail.com>
2023-09-29 23:56:39 +0530

    wrote unit test for Raffle.sol

--- smartContract-lottery-hardhat/contracts/Raffle.sol
+++ smartContract-lottery-hardhat/contracts/Raffle.sol
@@ -192,6 +192,10 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         return s_lastTimeStamp;
     }
 
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
+
     
 
     // function checkUpkeep(
